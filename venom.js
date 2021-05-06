c809f13e9888040a127c2ada76f9c6ae4e8469a4
Junior José <contato@autopyweb.com>
2021-05-06 05:33:32 -0300

    whatsapp Update

--- venom.js
+++ venom.js
@@ -4,7 +4,21 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 require('./src/Routes/index')(app); // <--- basta adicionar essa linha
+var porta = process.env.PORT || 8080;
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+	//console.log('conectado')
+	socket.on('pedido', (teste) => {
+		io.emit('pizzaria', teste);
+	})
+	socket.on('pedidostatus', (teste) =>{
+		io.emit('pedidostatus', teste)
+	})
+});
 
 app.use(cors());
 app.use(express.json());
-app.listen(3333);
+app.listen(porta);
