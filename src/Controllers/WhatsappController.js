4a632c6239fe817de5e2f5ef8694d3c1ad22c36c
Junior José <contato@autopyweb.com>
2021-05-09 17:43:27 -0300

    update

--- src/Controllers/WhatsappController.js
+++ src/Controllers/WhatsappController.js
@@ -1,22 +1,12 @@
 const startBot = require('../Services')
 var client
-startBot.then(venom =>{ client = venom; teste(venom)})
+startBot
+.then(venom =>{ client = venom; teste(venom)})
+.catch((error) => console.log(error));
 
 
 function teste(client){
-client.onMessage(async message => {
-  await client.sendText(message.from, 
-`Obrigado pela Mensagem ! 😌
-Mas sou somente um BOT feito para automação.👷🏽
-Caso Queira, automatizar seu negocio entre em contato com meu criador. 🙋🏽‍♂️.
-pelo nosso Email de contato ✉️: contato@autopyweb.com
-ou por telefone 📞: (12)3600-5005
 
-------------------------------------------------------------------
-Nossos Parceiros.
-🍕 - D'napolli Pizzaria e Lanchonete. (12)38967100
-`);
-});
 }
 
 exports.sendText = (req, res, next) => {
@@ -25,7 +15,7 @@ exports.sendText = (req, res, next) => {
   client.sendText(to, content).then((result) => {
     res.status(201).json({success: true, response: result.status})
   }).catch((erro) => {
-    res.status(401).json({success: false, error: erro.text})
+    res.status(201).json({success: false, error: erro.text})
   });
   
 };
--- src/Routes/WhatsappRoute.js
+++ src/Routes/WhatsappRoute.js
@@ -3,5 +3,4 @@ const WhatsappController = require('../Controllers/WhatsappController');
 module.exports = (app) => {
    app.post('/sendText', WhatsappController.sendText);
    app.post('/sendImage', WhatsappController.sendImage);
-
 }
