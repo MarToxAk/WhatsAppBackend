51c0700e6cbec4d0291c34f7c690b94b9b175d42
Junior José <contato@autopyweb.com>
2021-05-17 20:37:46 -0300

    make it better

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,7 +1,10 @@
 const venom = require('venom-bot');
+require('dotenv').config()
+
+console.log(`"${process.env.WATOKEN22}"`)
 
 module.exports = venom.create(
-    "Auto Py Web - Bot",
+"Auto Py Web - Bot",
     (base64Qrimg, asciiQR, attempts, urlCode) => {
       console.log('Number of attempts to read the qrcode: ', attempts);
       console.log('Terminal qrcode: ', asciiQR);
@@ -14,15 +17,7 @@ module.exports = venom.create(
       console.log('Session name: ', session);
     },
     {
-      folderNameToken: 'tokens', //folder name when saving tokens
-      mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
-      headless: true, // Headless chrome
-      devtools: false, // Open devtools by default
       useChrome: false, // If false will use Chromium instance
-      debug: false, // Opens a debug session
-      logQR: true, // Logs QR automatically in terminal
-      browserWS: '', // If u want to use browserWSEndpoint
-      browserArgs: [''], // Parameters to be added into the chrome browser instance
       puppeteerOptions: {
         executablePath: '/app/.apt/usr/bin/google-chrome'
       }, // Will be passed to puppeteer.launch
@@ -30,16 +25,9 @@ module.exports = venom.create(
       disableWelcome: true, // Will disable the welcoming message which appears in the beginning
       updatesLog: true, // Logs info updates automatically in terminal
       autoClose: 0, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-      createPathFileToken: false, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+      createPathFileToken: true, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
     },
-    {
-      WABrowserId: `"${process.env.WABROWSERID}"`,
-      WASecretBundle: `${process.env.WASECRETBUNDLE}`,
-  
-      
-      WAToken1: `"${process.env.WATOKEN1}"`,
-      WAToken2: `"${process.env.WATOKEN2}"`,
-    }
+    
 )
 
 
