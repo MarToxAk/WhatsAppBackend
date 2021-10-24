2c13f37317ea69c35ab23b96630e29b3ae092b61
Junior José <contato@autopyweb.com>
2021-10-23 21:23:46 -0300

    teste

--- src/Controllers/WhatsappController.js
+++ src/Controllers/WhatsappController.js
@@ -42,37 +42,41 @@ exports.sendText = (req, res, next) => {
 exports.sendPub = async (req, res, next) => {
   const db = await connectToDatabase(process.env.MONGODB_URI)
   const collection = await db.collection('users')
-  const { to, content } = req.body.args
   const users = await collection.find({}).toArray()
+  const success = 0
+  const error = 0
 
   for (const user in users) {
-    await client.sendImage(
-      `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
-      'https://i.ibb.co/j65yLrn/47.png',
-      'Propaganda',
-      `Boa Noite ${user.name}, Peça já seu Lanche Delicioso pelo nosso Aplicativo https://dnapolli.delivery/`
-    )
-    await client.sendText(
-      `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
-      `Se caso preferir Sr.ª  ${user.name}, Peça pelo nosso WhatsApp rapido e facil https://wa.me/551238967100`
-    )
-    await client.sendContactVcard(
-      `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
-      '551238967100@c.us'
-    )
+    await client
+      .sendImage(
+        `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
+        'https://i.ibb.co/j65yLrn/47.png',
+        'Propaganda',
+        `Boa Noite ${user.name}, Peça já seu Lanche Delicioso pelo nosso Aplicativo https://dnapolli.delivery/`
+      )
+      .then(() => {})
+      .catch(() => {})
+    await client
+      .sendText(
+        `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
+        `Se caso preferir Sr.ª  ${user.name}, Peça pelo nosso WhatsApp rapido e facil https://wa.me/551238967100`
+      )
+      .then(() => {})
+      .catch(() => {})
+    await client
+      .sendContactVcard(
+        `55${user.telephone.replace(/[^0-9]/g, '')}@c.us`,
+        '551238967100@c.us'
+      )
+      .then(() => {
+        success = +1
+      })
+      .catch(() => {
+        error = +1
+      })
   }
-  const buttons = [
-    { buttonId: 'id1', buttonText: { displayText: 'Venom 1' }, tipo: 1 },
-    { buttonId: 'id2', buttonText: { displayText: 'Venom 2' }, tipo: 1 },
-  ]
-  await client
-    .sendButtons('5512982062736@c.us', 'teste', buttons, 'test2')
-    .then((result) => {
-      res.status(201).json({ success: true, response: result.status })
-    })
-    .catch((erro) => {
-      res.status(201).json({ success: false, error: erro.text })
-    })
+
+  res.status(201).json({ success, error })
 }
 
 exports.sendImage = (req, res, next) => {
